47a1e6f004dfb94e389b64ba3223ad1c8cbd63c7
Larmeal <armloveyou252@hotmail.com>
2022-05-02 20:19:21 +0700

    .

--- sort_in_ascending.py
+++ sort_in_ascending.py
@@ -1,17 +1,16 @@
 import numpy as np
 
 
-array = np.random.randint(0, 50, 12)
+array = np.random.randint(0, 50, 10)
 array_list = array.tolist()
 
 print(array_list)
 
+
 def ascending(arr):
     new_list = []
     i = 0
-    
     while i < len(arr):
-        
         num = arr[0]
         for j in range(len(arr)):
             if num >= arr[j]: 
@@ -21,5 +20,12 @@ def ascending(arr):
 
     return new_list
 
+def bubbleSort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
 
 print(ascending(array_list))
+print(bubbleSort(array_list))
